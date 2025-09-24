da9f3b18742532777cd1c6011fdf3383bf91433d
dm-morozov <dem.morozov@gmail.com>
2025-09-24 18:36:41 +0300

    Подготовили классы ООП для проекта. Составили структуру проекта. Приступаем к реализации MessagesApi

---
+++ src/ts/MessagesApi.ts
@@ -0,0 +1,8 @@
+import { ServerMock } from "./server-mock";
+
+export default class MessagesApi {
+  // класс для работы с API
+  // Этот класс будет отвечать за выполнение
+  // запросов к серверу (в нашем случае к ServerMock)
+  //  с использованием rxjs/ajax
+}
---
+++ src/ts/MessagesWidget.ts
@@ -0,0 +1,8 @@
+// src/ts/MessagesService.ts
+
+export default class MessagesService {
+  // класс для UI (MessagesWidget):
+  // Этот класс будет отвечать за отображение
+  // таблицы сообщений и добавление новых
+  // сообщений сверху.
+}
